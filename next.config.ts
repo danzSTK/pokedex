46f0833f5418e0b481fc6f0f2b5b1f343cb79f42
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-08-31 21:25:26 -0300

    feat(config): atualizar configuração de imagens e adicionar script de rede
    - Alterado a configuração de imagens no arquivo next.config.ts para usar remotePatterns ao invés de domains.
    - Adicionado o script "dev:network" no arquivo package.json para permitir o desenvolvimento em rede.
    - Criado o arquivo usePokemonInfo.ts como um novo hook.
    
    Essas alterações melhoram a flexibilidade na configuração de imagens e facilitam o desenvolvimento em ambientes de rede, contribuindo para uma melhor experiência de desenvolvimento e integração com a aplicação.

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
 
   images: {
-    domains: ["raw.githubusercontent.com", "*"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 

fc2c8e9a58f73c4a409414178d6f3756f459bb71
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-09-01 11:16:26 -0300

    feat(view): reorganizar importações e ajustar estilo do cabeçalho
    - Adicionada uma nova seção de comentários para melhor organização do código.
    - Alterada a importação do componente FavoritesView para o caminho correto.
    - Modificado o estilo do componente Header para incluir uma borda inferior.
    
    Essas mudanças melhoram a legibilidade do código e garantem que os componentes sejam importados corretamente, contribuindo para uma estrutura mais clara e organizada da página de favoritos.

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+// Components
 import Header from "@/components/header";
 import { ThemeToggle } from "@/components/theme-toggle";
-import { FavoritesView } from "@/components/favorites/favorites-view";
 import MobileNavigationbar from "@/components/mobile-navigation";
+import { FavoritesView } from "@/view/favoritesSections/favorites-view";
 
 const FavoritesPage = () => {
   return (
     <div className="min-h-screen bg-background">
-      <Header className="py-8">
+      <Header className=" border-b">
         <div className="container mx-auto px-4 flex items-center justify-between">
           <h1 className="text-3xl font-bold tracking-tight">Pokédex</h1>
           <ThemeToggle />

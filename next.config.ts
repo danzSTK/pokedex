75fd1f430a6fd0a2113cc2b86274a97653183400
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-09-01 11:18:25 -0300

    feat(config): adicionar suporte a CORS e ajustar scripts de desenvolvimento
    - Adicionada função `headers` no arquivo `next.config.ts` para permitir acesso de qualquer host na rede local.
    - Atualizados os scripts no `package.json` para usar `--hostname` em vez de `-H` para os comandos de desenvolvimento e início.
    
    Essas alterações melhoram a configuração do projeto, permitindo que a aplicação seja acessada de diferentes hosts durante o desenvolvimento, facilitando testes em ambientes de rede local e colaborativos.

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,26 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  // Permitir acesso de qualquer host (para rede local)
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
